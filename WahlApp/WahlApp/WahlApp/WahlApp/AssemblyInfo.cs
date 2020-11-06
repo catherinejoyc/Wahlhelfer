da14b60b14614b166a26864796cd72301edfff38
Cat <cat.calupas@gmail.com>
2020-11-06 20:22:22 +0100

    add new proj "WahlApp" implement design (not finished yet)

---
+++ WahlApp/WahlApp/WahlApp/WahlApp/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Comfortaa-VariableFont_wght.ttf", Alias = "Comfortaa")]
+[assembly: ExportFont("GOTHIC.TTF", Alias = "Century Gothic")]
